37da8a7af34b578fc214e1195948ae0b91ff18ac
pelillian <plillian@usc.edu>
2020-12-17 10:51:59 -0800

    Update resume experience

--- pages/resume.vue
+++ pages/resume.vue
@@ -4,6 +4,15 @@
       <h1>RESUME</h1>
       <links />
       <h2>PROJECTS &amp; EXPERIENCE</h2>
+      <div
+        v-for="item in experience.data"
+        :key="item.info"
+        class="item"
+      >
+        <p><b>{{ item.org }}</b><span class="right">{{ item.when }}</span></p>
+        <span>{{ item.what }}</span><span class="dim right">{{ item.where }}</span>
+        <p v-html="item.info"></p>
+      </div>
     </div>
   </div>
 </template>
@@ -12,7 +21,6 @@
 export default {
   async asyncData({ $content, params }) {
     const experience = await $content('resume/experience').fetch()
-    console.log(experience)
     return { experience }
   },
   head() {
@@ -22,3 +30,21 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.item {
+  font-weight: normal;
+  text-align: left;
+  margin-top: 48px;
+  p {
+    margin-top: 18px;
+    b {
+      font-size: 24px;
+    }
+  }
+}
+
+.dim {
+  font-size: 16px;
+}
+</style>
