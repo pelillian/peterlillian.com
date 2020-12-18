c958c068e11664991b9b772a5ef1ef926521298c
pelillian <plillian@usc.edu>
2020-12-17 22:14:54 -0800

    Implement error page

---
+++ layouts/error.vue
@@ -0,0 +1,30 @@
+<template>
+  <transition name="page">
+    <div class="container">
+      <div>
+        <h1 v-if="error.statusCode === 404">PAGE NOT FOUND</h1>
+        <h1 v-else>AN ERROR OCCURED</h1>
+        <h1>{{ error.statusCode }}</h1>
+        <br>
+        <div class="links">
+          <NuxtLink
+            to="/"
+            class="button"
+            data-after="HOME PAGE"
+          >HOME PAGE</NuxtLink>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  }
+}
+</script>
